0b0751123b8c44e907ac770de9bce40fc016d67c
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-10-11 14:22:10 -0600

    get homepage

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
+});
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3002;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("HELLO WORLD!");
-})
+app.use('*', htmlRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`API server is now on port ${PORT}!`);
